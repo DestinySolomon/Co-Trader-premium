89ba7dea48feec3c95fe7eac5cf8065c45d9b247
DestinySolomon <deesystems247@gmail.com>
2025-10-28 19:23:16 +0100

    updated the transform tradind section

--- script.js
+++ script.js
@@ -24,3 +24,23 @@
             }
         });
    
+
+        
+        // Back to Top Button
+        const backToTopButton = document.getElementById('backToTop');
+        
+        window.addEventListener('scroll', function() {
+            if (window.scrollY > 300) {
+                backToTopButton.classList.add('show');
+            } else {
+                backToTopButton.classList.remove('show');
+            }
+        });
+        
+        backToTopButton.addEventListener('click', function() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+   
